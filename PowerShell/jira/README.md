a2be291d8a8e04957ac9eda8f491ed6a35a09ff4
Andrew Arace <andrew.arace@gmail.com>
2025-09-05 11:15:21 -0400

    rearranged folders, added subfolder README files, added Jira

---
+++ PowerShell/jira/README.md
@@ -0,0 +1,9 @@
+# Atlassian Jira Scripts
+Powershell scripts for interacting with Atlassian Jira with the [Aberrant](https://www.aberrant.io/) RemoteAgent
+
+## Contents
+
+| Script| Description |
+| -------- | ------- |
+| create_jira_ticket.ps1  | Creates a ticket in Jira with given name and description. Requires configuration of Email Address (ticket owner), API Token, Jira project and Url. |
+|  |  |
---
+++ PowerShell/jira/create_jira_ticket.ps1
@@ -0,0 +1,110 @@
+<#
+.SYNOPSIS
+    Creates a Jira ticket using the Jira REST API.
+
+.DESCRIPTION
+    This script takes a ticket name (summary) and a description as parameters and creates a new issue in a specified Jira project.
+
+.PARAMETER TicketName
+    The summary or title of the Jira ticket. This is a mandatory parameter.
+
+.PARAMETER TicketDescription
+    The description for the Jira ticket. This is a mandatory parameter.
+
+.EXAMPLE
+    .\create-jira-ticket.ps1 -TicketName "Fix the login button" -TicketDescription "The login button on the main page is not working when clicked. It needs to be investigated."
+
+    This command will create a new Jira ticket with the specified summary and description.
+#>
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $true, HelpMessage = "Enter the name/summary for the Jira ticket.")]
+    [string]$TicketName,
+
+    [Parameter(Mandatory = $true, HelpMessage = "Enter the description for the Jira ticket.")]
+    [string]$TicketDescription
+)
+
+# --- USER CONFIGURATION ---
+# IMPORTANT: Update these variables with your Jira instance details.
+
+# Your full Jira URL (e.g., https://aberrant.atlassian.net)
+$jiraUrl = "https://aberrant.atlassian.net"
+
+# The key for your Jira project (e.g., "PROJ", "TEST")
+$projectKey = "AB"
+
+$email = "andrew.arace@gmail.com"
+$apiToken = "ATATT3xFfGF04kCpzcnTHDi3EDJO5qhq_P3Ijy8scu4DZ9YiEV6xsWVtsDdSXAjtNMLUWpMK4T64s2MoCXZYkmdrFdlSDx7ysNuxy0pW9ofA1IHyVnuvGQ8rGXk2siFPoGkISB9gm3cMXP-Gbg_QR0YKJE0vBtu5P0oBDCvVPUp7mroM1IhGD1g=5A75205E"
+
+# The name of the issue type you want to create (e.g., "Task", "Story", "Bug", "Epic")
+$issueTypeName = "Task"
+
+# --- END OF USER CONFIGURATION ---
+
+try {
+    # Create the Base64 authentication header
+    $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $email, $apiToken)))
+    $headers = @{
+        "Authorization" = "Basic $base64AuthInfo"
+        "Content-Type"  = "application/json"
+    }
+
+    # Construct the JSON payload for the Jira API
+    # The structure must match what the Jira API expects for creating an issue.
+    $body = @{
+        fields = @{
+            project     = @{
+                key = $projectKey
+            }
+            summary     = $TicketName
+            description = $TicketDescription
+            issuetype   = @{
+                name = $issueTypeName
+            }
+        }
+    } | ConvertTo-Json
+
+    # Define the API endpoint for creating an issue
+    $apiUrl = "$jiraUrl/rest/api/2/issue"
+    
+    # Call the Jira REST API
+    $response = Invoke-RestMethod -Uri $apiUrl -Method Post -Headers $headers -Body $body
+
+    if ($response) {
+        $ticketUrl = "$($jiraUrl)/browse/$($response.key)"
+
+        # The manifest of the things this script will output
+        # for uploading later
+        $OutputManifest = @{
+            Files = @()
+            Links = @($ticketUrl)
+        }
+        # Return any output parameters this script has to pass on 
+        # as defined by the contracts.config.json
+        $OutputParameters = @{
+            TicketNumber = $response.key
+            TicketUrl = "$($jiraUrl)/browse/$($response.key)"
+        }
+
+        # Create the output wrapper object, wrapping the parameters and the Manifest
+        $ScriptOutput = @{
+            Parameters = $OutputParameters
+            Manifest = $OutputManifest
+        }
+
+        # write the output as json
+        Write-Output $ScriptOutput | ConvertTo-Json
+    }
+}
+catch {
+    Write-Error "An error occurred while creating the Jira ticket. $($_.Exception.Message)"
+
+    # Attempt to get more detailed error info from the response
+    if ($_.Exception.Response) {
+        $errorResponse = $_.Exception.Response.GetResponseStream()
+        $streamReader = New-Object System.IO.StreamReader($errorResponse)
+        $errorBody = $streamReader.ReadToEnd()
+        Write-Error "Jira API Response: $errorBody"
+    }
+}
